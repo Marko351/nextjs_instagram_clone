37b18f11f3cf5f22fc305f14318db6e657b7e9c3
Marko351 <markodjuric351@gmail.com>
2022-12-16 10:29:45 +0100

    implement sticky header

--- components/Header.js
+++ components/Header.js
@@ -5,35 +5,41 @@ import { HomeIcon } from '@heroicons/react/24/solid';
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
-      {/* Left */}
-      <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png?20160616034027"
-          fill
-          className="object-contain"
-        />
-      </div>
-      <div className="cursor-pointer h-24 w-10 relative lg:hidden">
-        <Image src="https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg" fill className="object-contain" />
-      </div>
-      {/* Middle */}
-      <div className="relative mt-1">
-        <div className="absolute pt-2 pl-2">
-          <MagnifyingGlassIcon className="h-5 text-gray-500" />
+    <div className="shadow-sm border-b sticky top-0 bg-white z-30">
+      <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
+        {/* Left */}
+        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png?20160616034027"
+            fill
+            className="object-contain"
+          />
         </div>
-        <input type="text" placeholder="Search" className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md" />
-      </div>
+        <div className="cursor-pointer h-24 w-10 relative lg:hidden">
+          <Image src="https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg" fill className="object-contain" />
+        </div>
+        {/* Middle */}
+        <div className="relative mt-1">
+          <div className="absolute pt-2 pl-2">
+            <MagnifyingGlassIcon className="h-5 text-gray-500" />
+          </div>
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+          />
+        </div>
 
-      {/* Right */}
-      <div className="flex space-x-4 items-center">
-        <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-        <img
-          src="https://pickaface.net/gallery/avatar/unr_fakeeliot_181127_1929_w34iu.png"
-          alt="User Image"
-          className="h-10 rounded-full cursor-pointer"
-        />
+        {/* Right */}
+        <div className="flex space-x-4 items-center">
+          <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <img
+            src="https://pickaface.net/gallery/avatar/unr_fakeeliot_181127_1929_w34iu.png"
+            alt="User Image"
+            className="h-10 rounded-full cursor-pointer"
+          />
+        </div>
       </div>
     </div>
   );
