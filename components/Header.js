661fa9314062221693bcb1ff7d1471c4fd4962e4
Marko351 <markodjuric351@gmail.com>
2022-12-16 10:16:52 +0100

    add search bar to the header

--- components/Header.js
+++ components/Header.js
@@ -1,27 +1,31 @@
 import Image from 'next/image';
 import React from 'react';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 export default function Header() {
   return (
-    <div>
+    <div className="flex items-center justify-between max-w-6xl">
       {/* Left */}
-
-      <div className="flex items-center justify-between max-w-6xl">
-        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-          <Image
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png?20160616034027"
-            fill
-            className="object-contain"
-          />
+      <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png?20160616034027"
+          fill
+          className="object-contain"
+        />
+      </div>
+      <div className="cursor-pointer h-24 w-10 relative lg:hidden">
+        <Image src="https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg" fill className="object-contain" />
+      </div>
+      {/* Middle */}
+      <div className="relative mt-1">
+        <div className="absolute top-2 left-2">
+          <MagnifyingGlassIcon className="h-5 text-gray-500" />
         </div>
-        <div className="cursor-pointer h-24 w-10 relative lg:hidden">
-          <Image src="https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg" fill className="object-contain" />
-        </div>
-        <h1>Right Side</h1>
+        <input type="text" placeholder="Search" className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md" />
       </div>
 
-      {/* Middle */}
       {/* Right */}
+      <h1>Right Side</h1>
     </div>
   );
 }
