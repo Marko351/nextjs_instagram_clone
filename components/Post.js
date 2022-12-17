3443a6d8299cf481b418c66e058d73d66e7cff75
Marko351 <markodjuric351@gmail.com>
2022-12-17 12:18:29 +0100

    create post buttons and tailwind custom class

--- components/Post.js
+++ components/Post.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { EllipsisHorizontalIcon } from '@heroicons/react/24/outline';
+import { EllipsisHorizontalIcon, HeartIcon, ChatBubbleOvalLeftIcon, BookmarkIcon } from '@heroicons/react/24/outline';
 
 export default function Post({ id, img, userImg, caption, username }) {
   return (
@@ -13,6 +13,15 @@ export default function Post({ id, img, userImg, caption, username }) {
 
       {/* Post Image */}
       <img src={img} alt="" className="object-cover w-full" />
+
+      {/* Post Buttons */}
+      <div className="flex justify-between items-center px-4 pt-4">
+        <div className="flex space-x-4">
+          <HeartIcon className="btn-post" />
+          <ChatBubbleOvalLeftIcon className="btn-post" />
+        </div>
+        <BookmarkIcon className="btn-post" />
+      </div>
     </div>
   );
 }
